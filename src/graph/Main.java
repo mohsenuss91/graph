d87d25240ef7c484d87872404d926ee146d04a91
rapsad <devilzaba@gmail.com>
2013-03-16 12:37:19 +0100

    Dodano: pliki projektu, Graph.java, Main.java

---
+++ src/graph/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
+
+/**
+ *
+ * @author rapsad
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String input_file = args[0];
+        int graph_size = Integer.parseInt(args[1]);
+        
+        
+    }
+}
