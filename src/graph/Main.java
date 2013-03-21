27f3cbe3864987344bf3398c60870c396e2992f6
rapsad <devilzaba@gmail.com>
2013-03-21 02:24:31 +0100

    Main.java: dodano wczytywanie z pliku pojedyńczymi liczbami

--- src/graph/Main.java
+++ src/graph/Main.java
@@ -1,22 +1,46 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package graph;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StreamTokenizer;
 
 /**
  *
- * @author rapsad
+ * @author Dawid Półchłopek
  */
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String input_file = args[0];
         int graph_size = Integer.parseInt(args[1]);
+        int number = 0;
         
+        //TODO jak policzyć ładnie to "m"
+        int m = 0;
+        //liczba wierzchołków graph_size = n
+        //tutaj ma byc zastosowane wpisywanie do struktury grafowej
+        Graph data = new Graph(graph_size, m);
+        
+        FileReader fileReader = new FileReader(input_file);
+        
+        StreamTokenizer st = new StreamTokenizer(fileReader);
+
+        try {
+            while( (number = st.nextToken()) != StreamTokenizer.TT_EOF ){
+                System.out.println("liczba: "+ st.nval);
+            }
+        } catch (IOException e) {
+            System.out.println("BŁĄD ODCZYTU Z PLIKU!");
+            System.exit(2);
+        }
+
+        //bufferedReader.close();
         
     }
 }
