f9108b9130e2e9dddd89b1371d011fa91c6a63d6
rapsad <devilzaba@gmail.com>
2013-03-21 02:22:21 +0100

    Graph.java: dodano wczytywania z pliku pojedynczych liczb

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,13 +1,51 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package graph;
 
 /**
  *
- * @author rapsad
+ * @author Dawid Półchłopek
  */
 public class Graph {
+
+    private int[] head;
+    private List next[];
+    private int graph_size;
+
+    public Graph(int n, int m){
+        
+        head = new int[n];
+        next = new List[m];
+        //automatyczna alokacja tych wszystkich rzeczy
+        //ale nie wpisywania danych
+        this.graph_size = graph_size;
+    }
     
+    
+    
+    public double getVertex() {
+        return 1;
+    }
+    
+    public double getEdge() {
+        return 1;
+    }
+    
+    public double getNextEdge() {
+        return 1;
+    }
+    
+//    public double addElement(String twoNumbers) {
+//        
+//    }
+    
+    
+}
+
+class List {
+    public int value;
+    public List next;
+    
+    List(int value) {
+        this.value = value;
+        this.next = null;
+    }
 }
